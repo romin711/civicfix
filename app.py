92b3973f92cf8bc11e8a967329247ce34657d255
Romin Kevadiya <rominkevadiya@gmail.com>
2026-03-12 21:58:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 app.secret_key = "civicfix_secret_key"
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+    
 #  CONFIG 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "database", "civicfix.db")
